ca6303b41bb9daf42f62ac0f20065eebce9eb83d
blue-one1 <blue.one@kakaoent.com>
2023-02-01 19:50:28 +0900

    Splash 화면 구현

---
+++ Commentoon/Commentoon/Splash/SplashView.swift
@@ -0,0 +1,37 @@
+//
+//  SplashView.swift
+//  Commentoon
+//
+//  Created by 이청원 on 2023/02/01.
+//
+
+import SwiftUI
+
+struct SplashView: View {
+    // @State : 값 변경 시 body값 다시 그려짐, body내에서만 사용해야함
+    @State var isActive:Bool = false
+    
+    var body: some View {
+        VStack {
+            if self.isActive {
+                ContentView()
+            } else {
+                Text("Splash View")
+                    .font(Font.largeTitle)
+            }
+        }
+        .onAppear {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 2.5) {
+                withAnimation {
+                    self.isActive = true
+                }
+            }
+        }
+    }
+}
+
+struct SplashView_Previews: PreviewProvider {
+    static var previews: some View {
+        SplashView()
+    }
+}
