19a6a17850f1e186730d2a1c15994f2c5af1d41e
blue-one1 <blue.one@kakaoent.com>
2023-02-01 18:47:28 +0900

    Project 생성

---
+++ Commentoon/Commentoon/CommentoonApp.swift
@@ -0,0 +1,17 @@
+//
+//  CommentoonApp.swift
+//  Commentoon
+//
+//  Created by 이청원 on 2023/02/01.
+//
+
+import SwiftUI
+
+@main
+struct CommentoonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
